af7727eb5fabc470dced24d1386cc4b9c8eb1a52
Tayyab Fayyaz <fayyaztayyab684@gmail.com>
2025-12-07 20:28:53 +0500

    feat: Configure Docusaurus for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://example.com',
+  url: 'https://tayyabfayyaz.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/book/',
+  baseUrl: '/AI_book/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'example-org', // Usually your GitHub org/user name.
-  projectName: 'book', // Usually your repo name.
+  organizationName: 'tayyabfayyaz', // Usually your GitHub org/user name.
+  projectName: 'AI_book', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
